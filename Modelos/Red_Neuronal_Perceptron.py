7daa504ff52b42e04f4ee30922bec3ae8e642fcb
Leiner Viloria <vilorialeiner2002@gmail.com>
2024-11-10 11:30:20 -0500

    Se agrega el modelo faltante

---
+++ Modelos/Red_Neuronal_Perceptron.py
@@ -0,0 +1,6 @@
+from Utils import Utilities
+
+def create_red_neuronal_top_level():
+    window = Utilities.create_standar_top_level("Red neuronal", "Perceptron")
+
+RED_NEURONAL = {"label": "Usar la red neuronal", "action": create_red_neuronal_top_level}
--- Modelos/__init__.py
+++ Modelos/__init__.py
@@ -2,10 +2,12 @@ from .Knn import KNN
 from .Arbol_De_Decision import ARBOL_DE_DECISION
 from .Regresion_Lineal import REGRESION_LINEAL
 from .Regresion_Lineal_Multiple import REGRESION_LINEAL_MULTIPLE
+from .Red_Neuronal_Perceptron import RED_NEURONAL
 
 MODELOS = [
+    RED_NEURONAL,
     KNN,
-    ARBOL_DE_DECISION,
     REGRESION_LINEAL,
-    REGRESION_LINEAL_MULTIPLE
+    ARBOL_DE_DECISION,
+    REGRESION_LINEAL_MULTIPLE,
 ]
